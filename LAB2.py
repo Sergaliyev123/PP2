053498265f176fc79ee250fe2516ce6cd72247ce
seragaliyev <sergalievn026@gmail.com>
2024-09-24 11:45:50 +0600

    change

--- LAB2.py
+++ LAB2.py
@@ -229,6 +229,32 @@ thisdict = {
 mydict = thisdict.copy()
 print(mydict)
 
+#The pop() method removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+#The popitem() method removes the last inserted item 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+#The del keyword removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
 
 #Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
 child1 = {
