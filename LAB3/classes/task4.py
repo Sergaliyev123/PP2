d190393828ef4029b26a6349a7372e160849cd60
seragaliyev <sergalievn026@gmail.com>
2024-09-25 11:12:51 +0600

    change

---
+++ LAB3/classes/task4.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(self.x, self.y)
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist(self, x, y):
+        d = sqrt(pow((self.x - x), 2) + pow((self.y - y), 2))
+        print(d)
+
+
+figure1 = Point(4, 5)
+
+figure1.move(4, 5)
+figure1.show()
+
+figure1.dist(3, 4)
